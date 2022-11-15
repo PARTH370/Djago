abea5fd93e07d63ea8d23597409594999c96d754
PARTHKUMAR <parthchodvadiya370@gmail.com>
2022-11-15 16:41:35 +0530

    Url Duplicate

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -12,13 +12,12 @@ urlpatterns = [
         name="home_page",
     ),
     path("job-industry",FT_view.create_job_industries,name="job_industry"),
-path("create-industry",FT_view.add_industry,name="add_industry"),
-path("job-profile",FT_view.create_job_profile,name="job_profile"),
-path("add-job-profile",FT_view.add_job_profile,name="add_job_profile"),
-path('edit-job-profile',FT_view.edit_job_industries,name="edit_job_profile"),
-path('update-job-profile',FT_view.update_job_industries,name="update_job_profile"),
-path('get-job-name',FT_view.get_job_name,name="get_job_name"),
-    path("job-industry", FT_view.create_job_industries, name="job_industry"),
-    path("create-industry", FT_view.add_industry, name="add_industry"),
+    path("create-industry",FT_view.add_industry,name="add_industry"),
     path("delete-industry", FT_view.delete_industry, name="delete_job_profile"),
+    path("job-profile",FT_view.create_job_profile,name="job_profile"),
+    path("add-job-profile",FT_view.add_job_profile,name="add_job_profile"),
+    path('edit-job-profile',FT_view.edit_job_industries,name="edit_job_profile"),
+    path('update-job-profile',FT_view.update_job_industries,name="update_job_profile"),
+    path('get-job-name',FT_view.get_job_name,name="get_job_name")
+    
 ]
